5d8abda3bc175efd073c3e06ebac9a10d9ac5632
rudyluis <rudy.manzaneda@gmail.com>
2024-06-03 22:10:29 -0400

    03062024

--- analisis_bivariados.py
+++ analisis_bivariados.py
@@ -18,25 +18,25 @@ def AnalisisBivariado(st, session):
     # Reportes Bivariados
     with c1:
         st.write("### Comparación de la satisfacción general con Univalle por sexo")
-        fig = px.box(df, x='sexo', y='satisfaccion_general_univalle', title='Comparación de la Satisfacción General con Univalle por Sexo')
+        fig = px.box(df, x='sexo', y='satisfaccion_general_univalle', title='Comparación de la Satisfacción General con Univalle por Sexo',color='sexo',color_discrete_sequence=px.colors.qualitative.Alphabet)
         st.plotly_chart(fig)
     with c2:
         st.write("### Relación entre la edad y la satisfacción con la formación académica")
-        fig = px.scatter(df, x='edad', y='satisfaccion_formacion_academica', title='Relación entre Edad y Satisfacción con la Formación Académica')
+        fig = px.scatter(df, x='edad', y='satisfaccion_formacion_academica', title='Relación entre Edad y Satisfacción con la Formación Académica', color='edad', color_discrete_sequence=px.colors.qualitative.Set2)
         st.plotly_chart(fig)
     c3,c4=st.columns(2)
     with c3:
         st.write("### Comparación de la satisfacción con los docentes por carrera")
-        fig = px.box(df, x='carrera', y='satisfaccion_docentes', title='Comparación de la Satisfacción con los Docentes por Carrera')
+        fig = px.box(df, x='carrera', y='satisfaccion_docentes', title='Comparación de la Satisfacción con los Docentes por Carrera',color='carrera',color_discrete_sequence=px.colors.qualitative.Set1)
         st.plotly_chart(fig)
     with c4:
         st.write("### Relación entre el trabajo actual y la satisfacción con la carrera")
-        fig = px.box(df, x='trabaja_actualmente', y='satisfaccion_carrera', title='Relación entre el Trabajo Actual y la Satisfacción con la Carrera')
+        fig = px.box(df, x='trabaja_actualmente', y='satisfaccion_carrera', title='Relación entre el Trabajo Actual y la Satisfacción con la Carrera', color='trabaja_actualmente',color_discrete_sequence=px.colors.qualitative.Prism)
         st.plotly_chart(fig)
     c5,c6=st.columns(2)
     with c5:
         st.write("### Comparación de la satisfacción con la infraestructura por sede")
-        fig = px.box(df, x='sede', y='satisfaccion_infraestructura', title='Comparación de la Satisfacción con la Infraestructura por Sede')
+        fig = px.box(df, x='sede', y='satisfaccion_infraestructura', title='Comparación de la Satisfacción con la Infraestructura por Sede',color='sede',color_discrete_sequence=px.colors.qualitative.Pastel)
         st.plotly_chart(fig)
     with c6:
         st.write("### Relación entre el plan de posgrado y la carrera")
@@ -49,9 +49,9 @@ def AnalisisBivariado(st, session):
         st.plotly_chart(fig)
     with c8:
         st.write("### Relación entre la ciudad de procedencia y la carrera")
-        fig = px.box(df, x='ciudad', y='carrera', title='Relación entre la Ciudad de Procedencia y la Carrera')
+        fig = px.box(df, x='ciudad', y='carrera', title='Relación entre la Ciudad de Procedencia y la Carrera',color='ciudad',color_discrete_sequence=px.colors.qualitative.Dark2)
         st.plotly_chart(fig)
         
     st.write("### Comparación de la satisfacción con la atención administrativa por si trabaja actualmente o no")
-    fig = px.box(df, x='trabaja_actualmente', y='satisfaccion_atencion_administrativa', title='Comparación de la Satisfacción con la Atención Administrativa por Si Trabaja Actualmente o No')
+    fig = px.box(df, x='trabaja_actualmente', y='satisfaccion_atencion_administrativa', title='Comparación de la Satisfacción con la Atención Administrativa por Si Trabaja Actualmente o No', color='trabaja_actualmente',color_discrete_sequence=px.colors.qualitative.Alphabet)
     st.plotly_chart(fig)
--- analisis_dinamico.py
+++ analisis_dinamico.py
@@ -20,7 +20,7 @@ def AnalisisDinamico(st, session):
     df['edad'] = df['edad'].fillna(df['edad'].mean())
     with c1: 
         st.subheader("Distribución de edades de los estudiantes")
-        fig = px.histogram(df, x='edad', nbins=20, title='Distribución de Edades')
+        fig = px.histogram(df, x='edad', nbins=20, title='Distribución de Edades', color_discrete_sequence=px.colors.qualitative.Alphabet)
         st.plotly_chart(fig)
     with c2:
         st.write("### Proporción de estudiantes por sexo")
@@ -28,7 +28,9 @@ def AnalisisDinamico(st, session):
         st.plotly_chart(fig)
 
     st.write("### Número de estudiantes por carrera")
-    fig = px.bar(df, x='carrera', title='Número de Estudiantes por Carrera')
+    ##fig = px.bar(df, x='carrera', title='Número de Estudiantes por Carrera',color_discrete_sequence=['#EF553B'])
+    fig = px.histogram(df, x='carrera', nbins=20, title='Número de Estudiantes por Carrera', color_discrete_sequence=px.colors.qualitative.Dark2)
+       
     st.plotly_chart(fig)
 
     c3,c4=st.columns(2)
@@ -36,13 +38,13 @@ def AnalisisDinamico(st, session):
         st.write("### Número de estudiantes que trabajan actualmente")
         trabajos = filtro_all_generico(Trabajo, session)
         df_trabajos = pd.DataFrame(trabajos)
-        fig = px.pie(df, names='estado_trabaja', title='Número de Estudiantes que Trabajan Actualmente', hole=0.3)
+        fig = px.pie(df, names='estado_trabaja', title='Número de Estudiantes que Trabajan Actualmente', hole=0.3,color_discrete_sequence=['#00CC96'])
         st.plotly_chart(fig)
     with c4:
         st.write("### Distribución de satisfacción general con Univalle")
         satisfaccion = filtro_all_generico(SatisfaccionUnivalle, session)
         df_satisfaccion = pd.DataFrame(satisfaccion)
-        fig = px.histogram(df, x='satisfaccion_general_univalle', nbins=10, title='Distribución de Satisfacción General con Univalle')
+        fig = px.histogram(df, x='satisfaccion_general_univalle', nbins=10, title='Distribución de Satisfacción General con Univalle',color_discrete_sequence=['#AB63FA'])
         st.plotly_chart(fig)
 
     c5,c6=st.columns(2)
@@ -51,7 +53,7 @@ def AnalisisDinamico(st, session):
         programas = filtro_all_generico(ProgramasAcademicos, session)
         df_programas = pd.DataFrame(programas)
        ## fig = px.pie(df, names='cursos_postgrado', title='Número de Estudiantes que Planean Realizar un Curso de Posgrado', hole=0.3)
-        fig = px.histogram(df, x='cursos_postgrado', nbins=10, title='Número de Estudiantes que Planean Realizar un Curso de Posgrado')
+        fig = px.histogram(df, x='cursos_postgrado', nbins=10, title='Número de Estudiantes que Planean Realizar un Curso de Posgrado',color_discrete_sequence=['#19D3F3'])
        
         st.plotly_chart(fig)
     with c6:
@@ -61,9 +63,9 @@ def AnalisisDinamico(st, session):
     c7,c8=st.columns(2)
     with c7:
         st.write("### Número de estudiantes por país de procedencia")
-        fig = px.bar(df, x='pais', title='Número de Estudiantes por País de Procedencia')
+        fig = px.bar(df, x='pais', title='Número de Estudiantes por País de Procedencia',color_discrete_sequence=['#B6E880'])
         st.plotly_chart(fig)
     with c8:                  
         st.write("### Distribución de estudiantes por nivel de satisfacción con los docentes")
-        fig = px.histogram(df, x='satisfaccion_docentes', nbins=10, title='Distribución de Satisfacción con los Docentes')
+        fig = px.histogram(df, x='satisfaccion_docentes', nbins=10, title='Distribución de Satisfacción con los Docentes',color_discrete_sequence=px.colors.qualitative.Safe)
         st.plotly_chart(fig)
